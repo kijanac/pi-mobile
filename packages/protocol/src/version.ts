e4a58df6fbdbe62f75d76d790c417b79604e1117
Ki-Jana <40298496+kijanac@users.noreply.github.com>
2026-06-06 00:07:36 +0000

    Release 0.9.7

--- packages/protocol/src/version.ts
+++ packages/protocol/src/version.ts
@@ -1,4 +1,4 @@
-export const PRODUCT_VERSION = "0.9.6";
+export const PRODUCT_VERSION = "0.9.7";
 export const PROTOCOL_VERSION = 9;
-export const MIN_MOBILE_VERSION = "0.9.6";
+export const MIN_MOBILE_VERSION = "0.9.7";
 export const RECOMMENDED_MOBILE_VERSION = PRODUCT_VERSION;
